36c8cf8e52d0d9c7682e78c240910f49585104f1
Sbipul <bipulcroy121@gmail.com>
2023-05-02 02:12:57 +0600

    last commit for this module

--- dist/alis.js
+++ dist/alis.js
@@ -10,7 +10,20 @@ const woman = {
 };
 const isMaried = false;
 const calculate = (num1, num2, operation) => {
-    console.log(operation);
     return operation;
 };
 calculate(4, 5, (x, y) => x + y);
+const newDev = {
+    name: "bipul",
+    age: 28,
+    gf: true,
+    job: "yes",
+};
+function generateAdder(a) {
+    return function (b) {
+        return a + b;
+    };
+}
+const addTwo = generateAdder(2);
+console.log(addTwo(3));
+console.log(addTwo(5));
