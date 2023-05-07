a510f1a73aa6e62c796ecb4b95329c38e3958384
Sbipul <bipulcroy121@gmail.com>
2023-05-08 02:38:26 +0600

    static method

--- src/oop/staticClass.ts
+++ src/oop/staticClass.ts
@@ -1,13 +1,13 @@
 class Counter {
   static counter: number = 0;
-  increment(): number {
+  static increment(): number {
     return (Counter.counter = Counter.counter + 1);
   }
-  decrement(): number {
+  static decrement(): number {
     return (Counter.counter = Counter.counter - 1);
   }
 }
 const count = new Counter();
 const count2 = new Counter();
-console.log(count.increment());
-console.log(count2.decrement());
+console.log(Counter.increment());
+console.log(Counter.decrement());
