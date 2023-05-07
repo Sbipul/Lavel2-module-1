40da7837151eb554c7ccef1a3050ab2ada86d258
Sbipul <bipulcroy121@gmail.com>
2023-05-08 02:22:06 +0600

    access modifier setup

---
+++ src/oop/getterSetter.ts
@@ -0,0 +1,35 @@
+class Idcard {
+  readonly name: string;
+  private _id: number;
+  address: string;
+  private _nationality: boolean;
+  mark: number;
+  constructor(
+    name: string,
+    _id: number,
+    address: string,
+    _nationality: boolean,
+    mark: number
+  ) {
+    this.name = name;
+    this._id = _id;
+    this.address = address;
+    this._nationality = _nationality;
+    this.mark = mark;
+  }
+  get nationality(): boolean {
+    return this._nationality;
+  }
+  getDetails() {
+    console.log(
+      `my name is ${this.name} and my id is ${this._id} and i live in ${this.address}`
+    );
+  }
+  addMark(marks: number): number {
+    return this.mark + marks;
+  }
+}
+
+const myid = new Idcard("Bipul chandro roy", 2301028001, "Dhaka", true, 50);
+console.log(myid.addMark(5));
+console.log(myid.nationality);
