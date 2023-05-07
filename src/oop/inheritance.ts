1871297d72483f75539827d8a98f4760707292a4
Sbipul <bipulcroy121@gmail.com>
2023-05-08 01:29:05 +0600

    inheritance initiated

--- src/oop/inheritance.ts
+++ src/oop/inheritance.ts
@@ -1,12 +1,29 @@
 class Human {
-  constructor(public name: string, public profession: string) {
+  constructor(
+    public name: string,
+    public profession: string,
+    public age: number
+  ) {
     this.name = name;
     this.profession = profession;
+    this.age = age;
   }
   makeSleep(hour: number): string {
-    return `this person sleep ${hour} hour`;
+    return `This person name is ${this.name} and he sleeps ${hour} hour`;
   }
 }
-// class Student extends Human {
-
-// }
+class Student extends Human {
+  girlfriend: boolean;
+  constructor(
+    name: string,
+    profession: string,
+    age: number,
+    girlfriend: boolean
+  ) {
+    super(name, profession, age);
+    this.girlfriend = girlfriend;
+  }
+}
+const studentDetails = new Student("Bipul chandro roy", "developer", 28, false);
+const sleeping_time = studentDetails.makeSleep(5);
+console.log(sleeping_time);
